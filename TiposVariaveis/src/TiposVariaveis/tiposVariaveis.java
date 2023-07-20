982b1c421de9fb37aad3a0c0ea7fc7a05eb85247
Luis Felipe S Botta <118137149+FelipeBotta@users.noreply.github.com>
2023-07-20 16:58:00 -0300

    .

--- TiposVariaveis/src/TiposVariaveis/tiposVariaveis.java
+++ TiposVariaveis/src/TiposVariaveis/tiposVariaveis.java
@@ -3,8 +3,25 @@ package TiposVariaveis;
 public class tiposVariaveis {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		String meuNome = "Luis";
+		
+		double salarioMinimo = 2500;
+		
+		short numeroCurto = 1;
+		int numeroNormal = numeroCurto;
+		short numeroCurto2 = (short) numeroNormal; //casting
+		
+		int numero = 5;
+		
+		numero = 10;
+		
+		System.out.println(numero);
+		
+		
+		final double VALOR_DE_PI = 3.14; //final define que a variavel se torna uma constante, por convencao o nome dela é em caixa alta.
+		
+		VALOR_DE_PI = 10;
 	}
 
 }
