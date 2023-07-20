b7480290abf759e5f59971e41d662169f430555e
Luis Felipe S Botta <118137149+FelipeBotta@users.noreply.github.com>
2023-07-20 17:50:08 -0300

    a

---
+++ Operadores/src/Operadores/Ternario.java
@@ -0,0 +1,23 @@
+package Operadores;
+
+public class Ternario {
+
+	public static void main(String[] args) {
+		
+		 int a,b ;
+		 
+		 a = 6;
+		 
+		 b = 6;	
+		 
+		 String resultado = a==b ? "verdadeiro" : "falso"  ;
+		 
+		 
+		 
+		 System.out.println(resultado);
+			 
+			
+		
+	}
+
+}
