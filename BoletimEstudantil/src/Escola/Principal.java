ef5fc8a1d06fce3fe51267c5e0c680d88fa21b1a
Luis Felipe S Botta <118137149+FelipeBotta@users.noreply.github.com>
2023-07-20 15:33:42 -0300

    BoletimEstudantil

---
+++ BoletimEstudantil/src/Escola/Principal.java
@@ -0,0 +1,26 @@
+package Escola;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		
+		int mediaFinal = 5;
+		
+		if(mediaFinal <6) {
+			
+			System.out.println("Reprovado !");
+			
+		}	else if(mediaFinal ==6) {
+			
+			System.out.println("Segunda Chamada !");
+		} else {
+			
+			System.out.println("Aprovado ! ");
+		}
+		
+		
+		
+		
+	}
+
+}
