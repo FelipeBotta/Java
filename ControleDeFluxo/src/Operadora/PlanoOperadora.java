679d3f57730b4f40d6d87892d34ecff07edc98b0
Luis Felipe S Botta <118137149+FelipeBotta@users.noreply.github.com>
2023-07-24 15:41:09 -0300

    a

---
+++ ControleDeFluxo/src/Operadora/PlanoOperadora.java
@@ -0,0 +1,23 @@
+package Operadora;
+
+public class PlanoOperadora {
+
+	public static void main(String[] args) {
+		
+		String operadora = "B";
+		
+		switch(operadora) {
+		case "T":{
+			System.out.println("5Gb Youtube");
+		}
+		case "M":{
+			System.out.println("Whats e Instagram Gratis");
+					}
+		case "B":{
+			System.out.println("100 Min de ligacao");
+		}
+		}
+		
+	}
+
+}
