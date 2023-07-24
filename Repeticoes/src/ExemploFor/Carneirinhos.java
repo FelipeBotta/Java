05bd6e9405357ebd17ac196b53d6519dce4b4f35
Luis Felipe S Botta <118137149+FelipeBotta@users.noreply.github.com>
2023-07-24 15:58:22 -0300

    a

---
+++ Repeticoes/src/ExemploFor/Carneirinhos.java
@@ -0,0 +1,16 @@
+package ExemploFor;
+
+public class Carneirinhos {
+
+	public static void main(String[] args) {
+				
+		for ( int carneiros = 1 ; carneiros <=20; carneiros++) {
+			
+			System.out.println("Contando carneirinhos " + carneiros);
+			
+		}
+		
+		System.out.println("Gabriela dormiu...");
+	}
+
+}
