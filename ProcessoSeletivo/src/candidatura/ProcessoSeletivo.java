b0e9bc745c787c2b3f016845a2b2cb646a9b8014
Luis Felipe S Botta <118137149+FelipeBotta@users.noreply.github.com>
2023-07-24 22:50:24 -0300

    a

--- ProcessoSeletivo/src/candidatura/ProcessoSeletivo.java
+++ ProcessoSeletivo/src/candidatura/ProcessoSeletivo.java
@@ -1,17 +1,48 @@
 package candidatura;
 
+import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class ProcessoSeletivo {
 
 	public static void main(String[] args) {
-		imprimirSelecionados();
+		String [] candidatos = {"Felipe","Marcia","Julia","Paulo","Augusto"};
+		for(String candidato: candidatos) {
+			entrandoEmContato(candidato);
+			
+		}
 		
 		
 	}
 	
+	static void entrandoEmContato(String candidato){
+		int tentativasRealizadas = 1;
+		boolean continuarTentando = true ;
+		boolean atendeu = false;
+		do {
+			atendeu = atender();
+			
+			continuarTentando = !atendeu;
+			
+			if(continuarTentando) {
+				
+				tentativasRealizadas++;
+				
+			}else{
+				
+				System.out.println("Contato Realizado com Sucesso");
+			}
+			
+		}while(continuarTentando && tentativasRealizadas < 3);
+	}
+	
+	static boolean atender() {
+		
+		return new Random().nextInt(3)==1;
+	}
+	
 	static void imprimirSelecionados() {
-		String [] candidatos = {"Felipe","Marcia","Julia","Paulo","Augusto","Monica","Fabricio","Mirela","Daniela","Jordan"};
+		String [] candidatos = {"Felipe","Marcia","Julia","Paulo","Augusto"};
 		System.out.println("Imprimindo a lista de candidatos informando o indice do elemento");
 		for(int indice=0; indice < candidatos.length;indice++) {
 			System.out.println("O candidato de n " + indice + 1 + " eh " +candidatos[indice]);
