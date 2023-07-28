a908b9dcc4d6065887d9bc5a4fbd3d1f0ce10ca4
Luis Felipe S Botta <118137149+FelipeBotta@users.noreply.github.com>
2023-07-27 21:39:02 -0300

    a

--- ENUM/src/teste/EstadoBrasileiro.java
+++ ENUM/src/teste/EstadoBrasileiro.java
@@ -1,19 +1,25 @@
 package teste;
 
 public enum EstadoBrasileiro {
-	SAO_PAULO("SP","Sao Paulo"),
-	MINAS_GERAIS("MG","Minas Gerais"),
-	PARANA("PR","Parana"),
-	SANTA_CATARINA("SC","Santa Catarina"),
-	RIO_GRANDE_DO_SUL("RS","Rio Grande do Sul")
+	SAO_PAULO("SP","Sao Paulo", 11),
+	MINAS_GERAIS("MG","Minas Gerais", 35),
+	PARANA("PR","Parana", 41),
+	SANTA_CATARINA("SC","Santa Catarina", 53),
+	RIO_GRANDE_DO_SUL("RS","Rio Grande do Sul", 24)
 	;
 	
 	private String nome;
 	private String sigla;
+	private int ibge;
 	
-	private EstadoBrasileiro(String sigla, String nome) {
+	private EstadoBrasileiro(String sigla, String nome, int ibge) {
 		this.sigla = sigla;
 		this.nome = nome;
+		this.ibge = ibge;
+	}
+	
+	public int getIbge() {
+		return ibge;
 	}
 	
 	public String getSigla() {
