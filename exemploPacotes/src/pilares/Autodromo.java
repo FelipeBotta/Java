4d4b0fcc91d58f8929cd621625692eb11c3e0a76
Luis Felipe S Botta <118137149+FelipeBotta@users.noreply.github.com>
2023-07-30 20:28:55 -0300

    a

--- exemploPacotes/src/pilares/Autodromo.java
+++ exemploPacotes/src/pilares/Autodromo.java
@@ -4,16 +4,14 @@ public class Autodromo {
 	public static void main(String[] args) {
 		Carro bmw = new Carro();
 		bmw.setChassi("1337");
-		//bmw.ligar();
+		bmw.ligar();
 		
 		
 		Moto cb300 = new Moto();
 		cb300.setChassi("1234");
-		//cb300.ligar();
+		cb300.ligar();
 		
-		Veiculo coringa = cb300;
 		
-		coringa.ligar();
 				
 				
 	}
