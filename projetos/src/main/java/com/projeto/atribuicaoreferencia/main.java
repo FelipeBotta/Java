544d7a351ae7b7d27e42e961935cb665ab9fbb59
Luis Felipe S Botta <118137149+FelipeBotta@users.noreply.github.com>
2023-08-03 21:57:49 -0300

    A

--- projetos/src/main/java/com/projeto/atribuicaoreferencia/main.java
+++ projetos/src/main/java/com/projeto/atribuicaoreferencia/main.java
@@ -2,7 +2,25 @@ package com.projeto.atribuicaoreferencia;
 
 public class main {
     public static void main(String[] args) {
-        
+
+        int intA = 1;
+        int intB = intA;
+
+        System.out.println(" intA=" + intA + " intB=" + intB);
+
+        intA = 2;
+
+        System.out.println(" intA=" + intA + " intB=" + intB);
+
+        meuObj objA = new meuObj(1);
+        meuObj objB = objA;
+
+        System.out.println(" objA=" + objA + " objB=" + objB);
+
+        objA.setNum(2);
+
+        System.out.println(" objA=" + objA + " objB=" + objB);
+
     }
 
 }
