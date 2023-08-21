0f0074fbf3ef2c0864b7a101f91ab73e420c025e
Luis Felipe S Botta <118137149+FelipeBotta@users.noreply.github.com>
2023-08-21 15:47:29 -0300

    Update ArvoreBinaria.java

--- projetos/arvoreBinaria/arvorebinaria/src/main/java/one/digitalinovation/ArvoreBinaria.java
+++ projetos/arvoreBinaria/arvorebinaria/src/main/java/one/digitalinovation/ArvoreBinaria.java
@@ -114,10 +114,23 @@ public class ArvoreBinaria<T extends Comparable<T>> {
                 }
 
             }else if(atual.getNoDir() == null){
+                if(pai.getNoEsq() == atual){
+                    pai.setNoEsq(atual.getNoEsq());
+                }else{
+                    pai.setNoDir(atual.getNoEsq());
+                }
 
             }else if(atual.getNoEsq() == null){
+                if(pai.getNoEsq() == atual){
+                    pai.setNoEsq(atual.getNoDir());
+                }else{
+                    pai.setNoDir(atual.getNoDir());
+                }
+
 
             }else{
+
+                
 
             }
 
