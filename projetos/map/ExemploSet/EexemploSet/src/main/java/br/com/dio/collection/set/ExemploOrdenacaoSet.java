14b91e49d8ef9a3994f691e62cff16cf711a6d8b
Luis Felipe S Botta <118137149+FelipeBotta@users.noreply.github.com>
2023-08-25 15:57:49 -0300

    Update ExemploOrdenacaoSet.java

--- projetos/map/ExemploSet/EexemploSet/src/main/java/br/com/dio/collection/set/ExemploOrdenacaoSet.java
+++ projetos/map/ExemploSet/EexemploSet/src/main/java/br/com/dio/collection/set/ExemploOrdenacaoSet.java
@@ -1,14 +1,66 @@
 package br.com.dio.collection.set;
 
+import java.util.Objects;
+
 public class ExemploOrdenacaoSet {
 
     public static void main(String[] args) {
 
-        
+
 
 
     }
 
 
 
+}
+
+
+class Serie{
+
+    private String nome;
+    private String genero;
+    private Integer tempoEpisodio;
+
+    public Serie(String nome, String genero, Integer tempoEpisodio) {
+        this.nome = nome;
+        this.genero = genero;
+        this.tempoEpisodio = tempoEpisodio;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public Integer getTempoEpisodio() {
+        return tempoEpisodio;
+    }
+
+
+    @Override
+    public String toString() {
+        return "{" +
+                "nome='" + nome + '\'' +
+                ", genero='" + genero + '\'' +
+                ", tempoEpisodio=" + tempoEpisodio +
+                '}';
+    }
+
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) return true;
+        if (object == null || getClass() != object.getClass()) return false;
+        Serie serie = (Serie) object;
+        return Objects.equals(nome, serie.nome) && Objects.equals(genero, serie.genero) && Objects.equals(tempoEpisodio, serie.tempoEpisodio);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nome, genero, tempoEpisodio);
+    }
 }
