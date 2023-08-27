8e941e1486f6c1282c21d1a76839c61dbdf36703
Luis Felipe S Botta <118137149+FelipeBotta@users.noreply.github.com>
2023-08-27 14:04:32 -0300

    Update ExemploMap.java

--- projetos/map/ExemploMap/ExemploMap/src/main/java/br/com/dio/collection/map/ExemploMap.java
+++ projetos/map/ExemploMap/ExemploMap/src/main/java/br/com/dio/collection/map/ExemploMap.java
@@ -1,13 +1,55 @@
 package br.com.dio.collection.map;
 
+import java.util.*;
+
 public class ExemploMap {
 
     public static void main(String[] args) {
 
+        System.out.println("Crie um dicionário que relacione os modelos e seus respectivos consumos:");
+        Map<String, Double> carrosPopulares = new HashMap<>(){{
+            put("gol", 14.4);
+            put("uno", 15.6);
+            put("mobi", 16.1);
+            put("hb20", 14.5);
+            put("kwid", 15.6);
+        }};
+        System.out.println(carrosPopulares.toString());
 
+        System.out.println("Substitua o consumo do gol por 15,2 km/l");
+        carrosPopulares.put("gol",15.2);
+        System.out.println(carrosPopulares);
 
+        System.out.println("Confira se o modelo tucson está no dicionario: " + carrosPopulares.containsKey("tucson"));
 
-        
+        System.out.println("Exiba o consumo do uno: " + carrosPopulares.get("uno"));
+
+   //     System.out.println("Exiba o terceiro modelo adicionado: ");
+
+        System.out.println("Exiba os modelos: ");
+        Set<String> modelos = carrosPopulares.keySet();
+        System.out.println(modelos);
+
+        System.out.println("Exiba o consumo dos carros: ");
+        Collection<Double> consumos = carrosPopulares.values();
+        System.out.println(consumos);
+
+        System.out.println("Exiba o modelo mais economico e seu consumo");
+
+        Double consumoMaisEficiente = Collections.max(carrosPopulares.values());
+        Set<Map.Entry<String, Double>> entries = carrosPopulares.entrySet();
+        String modeloMaisEficiente = "";
+
+        for(Map.Entry<String, Double> entry : entries){
+            if(entry.getValue().equals(consumoMaisEficiente)){
+                modeloMaisEficiente = entry.getKey();
+                System.out.println("Modelo mais eficiente: " + modeloMaisEficiente + " - " + consumoMaisEficiente);
+
+            }
     }
 
 }
+}
+
+
+
