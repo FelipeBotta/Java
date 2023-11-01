b23ad14e4a0812419b96b533fa72aac1d1c72c71
Luis Felipe S Botta <118137149+FelipeBotta@users.noreply.github.com>
2023-10-31 22:42:37 -0300

    Update Exercucui2IOCaracter.java

--- I-O/InputOutput/src/main/java/br/com/dio/java/io/IOCharacter/Exercucui2IOCaracter.java
+++ I-O/InputOutput/src/main/java/br/com/dio/java/io/IOCharacter/Exercucui2IOCaracter.java
@@ -1,4 +1,35 @@
 package br.com.dio.java.io.IOCharacter;
 
+
+import java.io.*;
+import java.util.Scanner;
+
 public class Exercucui2IOCaracter {
+
+    public static void lerTecladoEscreverDocumento() throws IOException {
+
+        PrintWriter pw = new PrintWriter(System.out);
+        pw.println("Digite 3 recomendações de filmes: ");
+        pw.flush();
+
+        Scanner scan = new Scanner(System.in);
+        String line = scan.nextLine();
+
+        File f = new File("recomedacoes.txt");
+
+        BufferedWriter bw = new BufferedWriter(new FileWriter(f.getName()));
+
+        
+
+
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        lerTecladoEscreverDocumento();
+
+    }
+
+
+
 }
