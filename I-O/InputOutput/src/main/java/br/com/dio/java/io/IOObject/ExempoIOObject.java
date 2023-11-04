2f971ef3e37aadafbfdfd7ca954eb5b1c6b3ba6c
Luis Felipe S Botta <felipeex7@gmail.com>
2023-11-04 01:17:13 -0300

    Update

--- I-O/InputOutput/src/main/java/br/com/dio/java/io/IOObject/ExempoIOObject.java
+++ I-O/InputOutput/src/main/java/br/com/dio/java/io/IOObject/ExempoIOObject.java
@@ -6,7 +6,7 @@ public class ExempoIOObject {
 
     public static void serealizacao() throws IOException {
 
-        Gato gato = new Gato("Simba",5,"Rajado");
+        Gato gato = new Gato("Simba",5,"Rajado",true,false);
 
         File f = new File("gato");
 
@@ -26,12 +26,28 @@ public class ExempoIOObject {
 
     }
 
-    public static void desserializacao(String arquivo){
+    public static void desserializacao(String arquivo) throws IOException, ClassNotFoundException {
+
+        ObjectInputStream ois = new ObjectInputStream(new FileInputStream(arquivo));
+        Gato objetoGato = (Gato) ois.readObject();
+
+        System.out.printf("\n Nome............: %s\n ",objetoGato.getNome());
+        System.out.printf("Idade...........: %d\n ",objetoGato.getIdade());
+        System.out.printf("Cor.............: %s\n ",objetoGato.getCor());
+        System.out.printf("Castrado........: %s\n ",objetoGato.isCastrado());
+        System.out.printf("Ronrona.........: %s\n ",objetoGato.isRonrona());
+        System.out.println(objetoGato);
+
+        ois.close();
+
+
 
     }
 
-    public static void main(String[] args) throws IOException {
-        serealizacao();
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+       // serealizacao();
+        desserializacao("C:\\Users\\felip\\OneDrive\\Documentos\\GitHub\\Java\\I-O\\gato");
+
     }
 
 }
