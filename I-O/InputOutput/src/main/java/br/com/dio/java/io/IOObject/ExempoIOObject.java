77b2d81d84b75197421e710c681c0b85a1334f78
Luis Felipe S Botta <felipeex7@gmail.com>
2023-11-04 00:51:57 -0300

    update

--- I-O/InputOutput/src/main/java/br/com/dio/java/io/IOObject/ExempoIOObject.java
+++ I-O/InputOutput/src/main/java/br/com/dio/java/io/IOObject/ExempoIOObject.java
@@ -1,4 +1,37 @@
 package br.com.dio.java.io.IOObject;
 
+import java.io.*;
+
 public class ExempoIOObject {
+
+    public static void serealizacao() throws IOException {
+
+        Gato gato = new Gato("Simba",5,"Rajado");
+
+        File f = new File("gato");
+
+        ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(f.getName()));
+
+        oos.writeObject(gato);
+
+        PrintStream ps = new PrintStream(System.out);
+        ps.printf("Arquivo \"%s\" criado com sucesso! Tamanho '%d' bytes. ",f.getName(),f.length());
+
+        oos.close();
+        ps.close();
+
+
+
+
+
+    }
+
+    public static void desserializacao(String arquivo){
+
+    }
+
+    public static void main(String[] args) throws IOException {
+        serealizacao();
+    }
+
 }
--- I-O/InputOutput/src/main/java/br/com/dio/java/io/IOObject/Gato.java
+++ I-O/InputOutput/src/main/java/br/com/dio/java/io/IOObject/Gato.java
@@ -1,4 +1,39 @@
 package br.com.dio.java.io.IOObject;
 
 public class Gato {
+
+    private String nome;
+    private Integer idade;
+    private String cor;
+
+    public Gato(String nome, Integer idade, String cor) {
+        this.nome = nome;
+        this.idade = idade;
+        this.cor = cor;
+    }
+
+    public Gato() {
+
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public Integer getIdade() {
+        return idade;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    @Override
+    public String toString() {
+        return "Gato{" +
+                "nome='" + nome + '\'' +
+                ", idade=" + idade +
+                ", cor='" + cor + '\'' +
+                '}';
+    }
 }
