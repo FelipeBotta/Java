7103d9b280430d435326509568843e48d3be1fbe
Luis Felipe S Botta <felipeex7@gmail.com>
2023-11-03 23:58:06 -0300

    Update ExemploIOData.java

--- I-O/InputOutput/src/main/java/br/com/dio/java/io/IOData/ExemploIOData.java
+++ I-O/InputOutput/src/main/java/br/com/dio/java/io/IOData/ExemploIOData.java
@@ -1,8 +1,56 @@
 package br.com.dio.java.io.IOData;
 
+import java.io.*;
+import java.util.Scanner;
+
 public class ExemploIOData {
 
-    
+    public static void incluirProduto() throws IOException {
+
+        File f = new File("C:\\Users\\felip\\OneDrive\\Documentos\\GitHub\\Java\\I-O\\peca-de-roupa.bin");
+
+        PrintStream ps = new PrintStream(System.out);
+        ps.flush();
+
+      //  OutputStream os = new FileOutputStream(f.getPath());
+      //  DataOutputStream dos = new DataOutputStream(os);
+
+        DataOutputStream dos = new DataOutputStream(new FileOutputStream(f.getPath()));
+
+        Scanner scan = new Scanner(System.in);
+
+        ps.print("Nome da peça: ");
+        String nome = scan.nextLine();
+        dos.writeUTF(nome);
+
+        ps.print("Tamanho da Peça (P|M|G|U): ");
+        char tamanho = (char) System.in.read();
+        dos.writeChar(tamanho);
+
+        ps.print("Quantidade: ");
+        int quant = scan.nextInt();
+        dos.writeInt(quant);
+
+        ps.print("Preço unitário: ");
+        double preco = scan.nextDouble();
+        dos.writeDouble(preco);
+
+        dos.close();
+        scan.close();
+
+
+
+    }
+
+    public static void lerProduto(String arquivo){
+
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        incluirProduto();
+
+    }
 
 
 }
